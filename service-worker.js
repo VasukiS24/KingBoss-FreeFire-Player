f81c2906a9798d1f33f769cb1d92072ac37f79e8
VasukiS24 <vasukitamil2024@gmail.com>
2026-01-28 20:21:38 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,33 @@
+const CACHE_NAME = "kingboss-cache-v1";
+const urlsToCache = [
+  "/KingBoss-FreeFire-Player/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/banner.jpg",
+  "/map.jpg",
+  "/prize.jpg",
+  "/winner.jpg",
+  "/feature.jpg",
+  "/insta-icon.png",
+  "/date.png",
+  "/game.png",
+  "/entry.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("kingboss-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
